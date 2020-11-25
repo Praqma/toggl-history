1861fca620c17f9d95f1a5de2dd6025665057ae3
Thierry Lacour <thi@praqma.net>
2020-11-25 11:48:55 +0100

    Group down to task level

--- history/history.py
+++ history/history.py
@@ -24,7 +24,7 @@ def store(toggl, monthCount):
 def collapse_month_report(month_report):
     grouped_entries = defaultdict(list)
     for entry in month_report:
-        key = (entry["uid"], entry["pid"], entry["is_billable"])
+        key = (entry["uid"], entry["pid"], entry["tid"], entry["is_billable"])
         grouped_entries[key].append(entry)
 
     collapsed_entries = []
@@ -33,8 +33,6 @@ def collapse_month_report(month_report):
 
         entry_ids = list(map(lambda e: e["id"], entries))
         entry_ids.sort()
-        task_ids = list(map(lambda e: e["tid"] or -1, entries)) # -1 is a workaround for None task ids
-        task_ids.sort()
         duration = sum(map(lambda e: int(e["dur"]), entries))
 
         collapsed_entries.append(dict(
@@ -42,9 +40,10 @@ def collapse_month_report(month_report):
             user=first_entry["user"],
             pid=first_entry["pid"],
             project=first_entry["project"],
+            tid=first_entry["tid"],
+            task=first_entry["task"],
             is_billable=first_entry["is_billable"],
             dur=duration,
-            tids=",".join(str(tid) for tid in task_ids),
             ids=",".join(str(id) for id in entry_ids),
         ))
 
@@ -52,16 +51,16 @@ def collapse_month_report(month_report):
 
 
 def collapsed_report_to_csv(month_report):
-    raw_header = "user_id;project_id;is_billable;duration;task_id;entry_id\n"
-    pretty_header = "user;project;is_billable;duration\n"
+    raw_header = "user_id;project_id;task_id;is_billable;duration;entry_ids\n"
+    pretty_header = "user;project;task;is_billable;duration\n"
 
     raw_lines = []
     pretty_lines = []
     for entry in month_report:
         raw_lines.append(
-            f'{entry["uid"]};{entry["pid"]};{entry["is_billable"]};{entry["dur"]};{entry["tids"]};{entry["ids"]}')
+            f'{entry["uid"]};{entry["pid"]};{entry["tid"]};{entry["is_billable"]};{entry["dur"]};{entry["ids"]}')
         pretty_lines.append(
-            f'{entry["user"]};{entry["project"]};{entry["is_billable"]};{time.format_ms_as_hhmmss(entry["dur"])}')
+            f'{entry["user"]};{entry["project"]};{entry["task"]};{"billable" if entry["is_billable"] else "non-billable"};{time.format_ms_as_hhmmss(entry["dur"])}')
 
     raw_lines.sort()
     pretty_lines.sort()
