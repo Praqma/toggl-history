9d07e40defad63fabdcab4a64f6fcc64822b35fb
Thierry Lacour <thi@praqma.net>
2020-11-27 16:17:55 +0100

    Remove get_previous_month

--- utils/time.py
+++ utils/time.py
@@ -57,22 +57,6 @@ def get_month_range(start, until):
         ]
         return first_year + in_between_years + final_year
 
-def get_previous_month(given_day = date.today()):
-    """
-    Returns the first day of the previous month for the given day
-
-    >>> get_previous_month(date(1991, 1, 31))
-    datetime.date(1990, 12, 1)
-    """
-    month, year = -1, -1
-    if (given_day.month == 1):
-        month = 12
-        year = given_day.year -1
-    else:
-        month = given_day.month - 1
-        year = given_day.year
-    return date(year, month, 1)
-
 def get_month_day_range(day_in_month):
     """
     Returns the first and last day for the given day's month
